dc8d66cedc0c61cb01c4977975bd831682dfa079
unknown <phucuong200297@gmail.com>
2022-02-28 14:06:17 +0700

    fix port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const { request } = require('https')
-const port = process.env.PORT ||3000;
+
 const fs = require('fs');
 
 
@@ -19,6 +19,6 @@ app.get('/loc', (request, response) => {
   })
 
 
-app.listen(port,()=>{
-    console.log(`Running on port ${port}`)
+app.listen(process.env.PORT ||5000,()=>{
+    console.log(`Running on port 5000`)
 })
